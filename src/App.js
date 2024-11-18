8f5e7858b42a18509c917dec289f5773c8853f05
Andrzej Mochel <amochel@gmail.com>
2024-11-18 21:51:45 +0100

    Posprzatanie stsrych funkcjonalnosci

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import {Link, Route, Switch} from "react-router-dom";
 import './App.css';
 import {PrivateRoute} from "./components/PrivateRoute";
 import {Home} from "./components/Home";
-import {Login} from "./components/Login";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {Register} from "./components/SignUp";
 import {OrderRegister} from "./components/OrderRegister";
 import {OrderRegisterSuccess} from "./components/OrderRegisterSuccess";
-import {LoginLogoutButton} from "./components/LoginLogoutButton";
-import {ChangePassword} from "./components/ChangePassword";
 
 
 export default function App() {
@@ -21,16 +17,12 @@ export default function App() {
                 <div className="container">
                     <Link className="navbar-brand" to={"/login"}>Piggi</Link>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <LoginLogoutButton />
                     </div>
                 </div>
             </nav>
 
             <Switch>
                 <PrivateRoute exact path="/" component={Home}/>
-                <PrivateRoute exact path="/change-password" component={ChangePassword}/>
-                <Route path="/login" component={Login}/>
-                <Route path="/sign-up" component={Register}/>
                 <Route path="/order-register/:id" component={OrderRegister}/>
                 <Route path="/order-register-success/:id" component={OrderRegisterSuccess}/>
             </Switch>
